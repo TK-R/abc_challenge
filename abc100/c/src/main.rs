03c5a1472a8a4a23a2094c7ef0088c081563f2d3
Ryosuke Koichi <subtkr@gmail.com>
2018-06-18 21:18:54 +0900

    abc100 a/b/c

---
+++ abc100/c/src/main.rs
@@ -0,0 +1,87 @@
+use std::io;
+
+/// 一行を読み込んで、空白で区切ったベクタにして返す
+#[allow(dead_code)]
+fn read<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+/// n行を読み込んで、数字を空白で区切って二次元のベクタにして返す
+#[allow(dead_code)]
+fn read_table<T>(n: usize) -> Vec<Vec<T>>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut ret: Vec<Vec<T>> = Vec::new();
+    for _ in 0..n {
+        ret.push(read::<T>());
+    }
+    ret
+}
+
+/// n行を読み込んで、1文字ずつを二次元のベクタにして返す
+#[allow(dead_code)]
+fn read_char_table(n: usize) -> Vec<Vec<char>> {
+    let mut ret: Vec<Vec<char>> = Vec::new();
+    for _ in 0..n {
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
+        let chr = buf.trim().chars().collect();
+        ret.push(chr);
+    }
+    ret
+}
+
+/// n行を読み込んで、一次元のベクタにして返す
+#[allow(dead_code)]
+fn read_array<T>(n: usize) -> Vec<T>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut array = Vec::new();
+
+    for _ in 0..n {
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
+        array.push(buf.trim().parse().unwrap());
+    }
+    array
+}
+
+fn main() {
+    // 数値
+    let _n = {
+        let i = read::<usize>();
+        (i[0])
+    };
+
+    // 一列の数値
+    let array = read::<u64>();
+
+    let mut count = 0;
+
+    for a in array {
+        let mut aa = a;
+        'loop_label: loop {
+            if aa % 2 != 0 {
+                break 'loop_label;
+            }
+
+            count += 1;
+            aa = aa / 2;
+        }
+    }
+
+    println!("{}", count);
+}
